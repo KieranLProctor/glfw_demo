6948be30a5be34c1a0d47435bbbb335409582c3c
Kieran Proctor <kieran.lee.proctor@gmail.com>
2022-04-09 18:09:04 +0100

    There is now a triangle, the colour is broken though but will fix later

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
 #include <GLFW/glfw3.h>
 
+#include <iostream>
+
 GLFWwindow* WINDOW;
+
+static unsigned int compileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    return id;
+}
+
+static int createShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = compileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = compileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+} 
 
 int init()
 {
@@ -25,11 +52,52 @@ int main()
 {
     init();
 
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+
+    unsigned int buffer;
+    // TODO: These are deprecated so change.
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    std::string vertexShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(0) in vec4 position;"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}";
+
+    std::string fragmentShader = 
+        "#version 330 core\n"
+        "\n"
+        "layout(0) out vec4 colour;"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   colour = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}";
+
+    unsigned int shader = createShader(vertexShader, fragmentShader); 
+    glUseProgram(shader);
+
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(WINDOW))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(WINDOW);
